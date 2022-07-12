75a8753385d2efe9f95408346624beecb9ee6f93
Anthony Brown <ac524.brown@gmail.com>
2022-07-12 10:52:26 -0700

    Created form based search functionality - Added new `getCards` api method that can accept an object of query params to make dynamic requests to the /cards endpoint - Created html form elements needed to allow the user to input a card name, select rarity and select a specific set - Updated search.js to handle the form button click, handle using the getCards method and providing fetched data to get generated HTML and append it to the page.

--- js/mtgapi.js
+++ js/mtgapi.js
@@ -1,5 +1,26 @@
 var apiBaseUrl = "https://api.magicthegathering.io/v1/";
 
+function getCards( options ) {
+
+    // name
+    // rarity
+    // set
+
+    var getCardsUrl = apiBaseUrl + "/cards";
+
+    if( options ) {
+        var searchParams = new URLSearchParams(options);
+
+        getCardsUrl += "?" + searchParams.toString();
+    }
+
+    return fetch(getCardsUrl)
+        .then(function(response) {
+            return response.json();
+        });
+
+}
+
 function getCardsBySupertype( supertype ) {
 
     var getCardsUrl = apiBaseUrl + "/cards?supertypes=" + supertype;
--- js/search.js
+++ js/search.js
@@ -1,19 +1,63 @@
 var resultsEl = document.querySelector('#results');
-var buttonEl = document.querySelector('#get-cards-legendary');
-var button2El = document.querySelector('#get-cards-basic');
+var searchBtnEl = document.querySelector('#search-btn');
+var introEl = document.querySelector('#intro');
+var isIntroHidden = false;
 
-buttonEl.addEventListener("click", function() {
 
-    getCardsBySupertype("Legendary")
+var nameFieldEl = document.querySelector('#card-name-field');
+var rarityFieldEl = document.querySelector('#card-rarity-field');
+var setFieldEl = document.querySelector('#card-set-field');
+
+searchBtnEl.addEventListener("click", function() {
+
+    var nameValue = nameFieldEl.value;
+    var rarityValue = rarityFieldEl.value;
+    var setValue = setFieldEl.value;
+
+    var requestOptions;
+
+    if(nameValue || rarityValue || setValue) {
+        requestOptions = {};
+
+        if(nameValue) requestOptions.name = nameValue;
+        if(rarityValue) requestOptions.rarity = rarityValue;
+        if(setValue) requestOptions.set = setValue;
+    }
+
+    getCards(requestOptions)
         .then(function (data) {
+            if(!isIntroHidden) {
+                introEl.style.display = "none"
+                isIntroHidden = true;
+            }
             var cards = data.cards;
             var content = getCardsContent(cards);
             console.log(content);
             resultsEl.innerHTML = "";
             resultsEl.append(content);
         });
-
+    
 });
+
+
+
+// var buttonEl = document.querySelector('#get-cards-legendary');
+// var button2El = document.querySelector('#get-cards-basic');
+
+
+
+// buttonEl.addEventListener("click", function() {
+
+//     getCardsBySupertype("Legendary")
+        // .then(function (data) {
+        //     var cards = data.cards;
+        //     var content = getCardsContent(cards);
+        //     console.log(content);
+        //     resultsEl.innerHTML = "";
+        //     resultsEl.append(content);
+        // });
+
+// });
 
 // button2El.addEventListener("click", function() {
 //     getCardsBySupertype("Basic");
